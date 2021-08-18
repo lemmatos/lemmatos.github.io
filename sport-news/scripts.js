66149dffd9fcdfd3ca33420539a02b9556e5c24e
Leandro M <40582547+lemmatos@users.noreply.github.com>
2021-08-18 12:25:31 +0200

    Add a header to the main section (#2)
    * Initial commit
    
    * Add simple description and functionality
    
    * Change README Under Construction formating to list item
    
    * Fix fonts and mangins
    
    * Refactor code to separate DOM manipulation and data fetching
    
    * Add main header
    
    * Add main header

--- sport-news/scripts.js
+++ sport-news/scripts.js
@@ -3,29 +3,49 @@ const apiAddress = 'https://www.thesportsdb.com/api/v1/json/1'
 
 const body = document.querySelector("body")
 const title = document.querySelector("#title")
+const mainHeader = document.querySelector("#main_header")
 const content = document.querySelector("#content")
 
 let sports = []
 let leagues = []
 let teams = []
 
+const DATA_TYPES = {
+  SPORT: "SPORT",
+  LEAGUE: "LEAGUE",
+  TEAM: "TEAM",
+}
+
+// -----------------------------
+// DOM manipulation
+// -----------------------------
 
 const addContent = (text) => {
-  const newParagraph=document.createElement('p')
+  const newParagraph = document.createElement('p')
   text 
   ? newParagraph.append(text)
   : newParagraph.append(`This is paragraph ${content.children.length + 1}`)
   content.appendChild(newParagraph)
 }
 
+const clearMain = () => {
+  clearMainHeader()
+  clearContent()
+  appendMainHeader("Select one option to start...")
+}
+
+const clearMainHeader = () => {
+  const childCount = mainHeader.childElementCount
+  for (i=0 ; i < childCount ; i++){
+    mainHeader.lastChild.remove()
+  }
+}
 const clearContent = () => {
   const childCount = content.childElementCount
   for (i=0 ; i < childCount ; i++){
     content.lastChild.remove()
   }
 }
-
-
 
 const appendCard = (onClick, title, imageUrl) => {
   const newDiv = document.createElement('div')
@@ -47,45 +67,86 @@ const appendCard = (onClick, title, imageUrl) => {
   content.appendChild(newDiv)
 }
 
-const getAllSports = async () => {
+const appendMainHeader = (title, description) => {
+  const newH2 = document.createElement('h2')
+  const newDescription = document.createElement('p')
+
+  newH2.append(title)
+
+  if (description){
+    newDescription.append(description)
+  }
+
+  mainHeader.appendChild(newH2)
+  mainHeader.appendChild(newDescription)
+}
+
+const generateCards = (type) => {
+  console.log("Generating cards...")
+  console.log(type)
+  clearMainHeader()
   clearContent()
+  switch (type){
+    case DATA_TYPES.SPORT:
+      console.log(sports)
+      appendMainHeader("Sports")
+      sports.forEach(item => item.strSport && item.strSport && item.strSportThumb && 
+        appendCard(() => getLeagues(item.strSport), item.strSport,item.strSportThumb)
+      )
+      break;
+    case DATA_TYPES.LEAGUE:
+      console.log(leagues)
+      console.log(leagues[0].strSport)
+      if (leagues[0].strBadge){
+        appendMainHeader(leagues[0].strSport, sports.find(sport => sport.strSport === leagues[0].strSport).strSportDescription)
+        leagues.forEach(item => appendCard(() => getLeagueTeams(item.idLeague), item.strLeague, item.strBadge))
+      }
+      else {
+        appendMainHeader("All sport leagues")
+        leagues.forEach(item => appendCard(() => getLeagueTeams(item.idLeague), item.strLeague))
+      }
+    break;
+    case DATA_TYPES.TEAM:
+      console.log(teams)
+      appendMainHeader(teams[0].strLeague, leagues.find(league => league.idLeague === teams[0].idLeague).strDescriptionEN)
+      teams.forEach(item => appendCard(() => getLeagues(item.strSport), item.strTeam, item.strTeamBadge))
+      break;
+    default:
+      break;
+  }
+
+}
+
+// -----------------------------
+// Data fetching
+// -----------------------------
+
+const getAllSports = async () => {
+  
   if (sports.length > 0){
     console.log("Loading sports from cache")
-    console.log(sports)
-    sports.forEach(item => item.strSport && item.strSport && item.strSportThumb && 
-      appendCard(() => getAllLeagues(item.strSport), item.strSport,item.strSportThumb)
-    )
+    generateCards(DATA_TYPES.SPORT)
     return sports
   } 
   else {
-    const newSports = fetch(`${apiAddress}/all_sports.php`,
-      {
-      //  headers :{
-      //   "x-auth-token": "194378278e394272b97aceb6b9da3d19",
-      //  },
-      })
+    const newSports = fetch(`${apiAddress}/all_sports.php`, {})
       .then(res => {
           console.log("Fetching data", res)
           return res.json()
       })
       .then(data => {
-        console.log(data)
         sports = data.sports
-        data.sports.forEach(item => item.strSport && item.strSport && item.strSportThumb && 
-            appendCard(() => getAllLeagues(item.strSport), item.strSport,item.strSportThumb)
-        )
+        generateCards(DATA_TYPES.SPORT)
       })
       .catch(err => console.log("Something went wrong fetching the data...", err))
     return newSports
   }
 }
 
-const getAllLeagues = async (sportName) => {
-  clearContent()
+const getLeagues = async (sportName) => {
   if (leagues.length > 0 && leagues[0].strSport === sportName){
     console.log("Loading leagues from cache")
-    console.log(leagues)
-    leagues.forEach(item => appendCard(() => getLeagueTeams(item.idLeague), item.strLeague, item.strBadge))
+    generateCards(DATA_TYPES.LEAGUE)
     return leagues
   }
   else {
@@ -95,14 +156,13 @@ const getAllLeagues = async (sportName) => {
           return res.json()
         })
         .then(data => {
-          console.log(data)
           if (sportName){
             leagues = data.countrys
-            data.countrys.forEach(item => appendCard(() => getLeagueTeams(item.idLeague), item.strLeague, item.strBadge))
+            generateCards(DATA_TYPES.LEAGUE)
           }
           else {
             leagues = data.leagues
-            data.leagues.forEach(item => appendCard(() => getLeagueTeams(item.idLeague), item.strLeague))
+            generateCards(DATA_TYPES.LEAGUE)
           }
         })
         .catch(err => console.log("Something went wrong fetching the data...", err))
@@ -111,11 +171,9 @@ const getAllLeagues = async (sportName) => {
 }
 
 const getLeagueTeams = async (idLeague) => {
-  clearContent()
   if (teams.length > 0 && teams[0].idLeague === idLeague){
     console.log("Loading teams from cache")
-    console.log(teams)
-    teams.forEach(item => appendCard(getAllSports, item.strTeam, item.strTeamBadge))
+    generateCards(DATA_TYPES.TEAM)
     return teams
   }
   else {
@@ -125,10 +183,9 @@ const getLeagueTeams = async (idLeague) => {
           return res.json()
         })
         .then(data => {
-          console.log(data)
           if (idLeague){
             teams = data.teams
-            data.teams.forEach(item => appendCard(() => getAllLeagues(item.strSport), item.strTeam, item.strTeamBadge))
+            generateCards(DATA_TYPES.TEAM)
           }
           else {
             throw `League ID provided was "${idLeague}"`
